fca82336a987429a44f314931640abea9be44d99
Kotov Mikhail <Kotov@ecopsy.ru>
2025-03-10 19:37:53 +0300

    Remove test scripts entry from .gitignore

--- app/logging/logging.py
+++ app/logging/logging.py
@@ -1,11 +1,15 @@
 import logging
+from pathlib import Path
 
 import yaml
 
-CONFIG_PATH = "app/logging/logging.yaml"
+CONFIG_PATH = Path("app/logging/")
+
+if not CONFIG_PATH.exists():
+    CONFIG_PATH.mkdir(parents=True)
 
 
 def configure_logging():
-    with open(CONFIG_PATH) as f:
+    with open(CONFIG_PATH / "logging.yaml") as f:
         logging_config = yaml.safe_load(f)
         logging.config.dictConfig(logging_config)
